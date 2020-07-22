f7db7356537fc3d3f573852c5b7d10ca8c62f5c8
jnhtt <man.in.cyber.space@gmail.com>
2020-07-22 20:21:58 +0900

    Chapter1

---
+++ Assets/Chapter1/Chapter1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Chapter1 : MonoBehaviour
+{
+    private GameObject prefab;
+    private int counter;
+
+    private IEnumerator Start()
+    {
+        var op = Addressables.LoadAssetAsync<GameObject>("Cube");
+        yield return new WaitUntil(() => op.IsDone);
+        prefab = op.Result;
+    }
+
+    private void Update()
+    {
+        CreateCube();
+    }
+
+    private void CreateCube()
+    {
+        if (prefab == null)
+        {
+            return;
+        }
+
+        if (counter == 120)
+        {
+            counter = 0;
+            var go = GameObject.Instantiate(prefab);
+            go.transform.position = 5f * UnityEngine.Random.insideUnitSphere;
+        }
+        ++counter;
+    }
+}
